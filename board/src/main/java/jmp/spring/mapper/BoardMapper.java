8a56e94a26bd590a67cab6c40ea8711dc7d84fed
mika <73530023+2everlove@users.noreply.github.com>
2021-04-12 14:11:52 +0900

    BoardMapper insert implerments & Test

---
+++ board/src/main/java/jmp/spring/mapper/BoardMapper.java
@@ -0,0 +1,8 @@
+package jmp.spring.mapper;
+
+import jmp.spring.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	int insert(BoardVO board);
+}
---
+++ board/src/test/java/jmp/spring/mapper/BoardMapperTests.java
@@ -0,0 +1,31 @@
+package jmp.spring.mapper;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import jmp.spring.controller.Test;
+import jmp.spring.domain.BoardVO;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper boardMapper;
+	
+	@org.junit.Test
+	public void insertTest() {
+		BoardVO board = new BoardVO();
+		
+		board.setTitle("Test 제목");
+		board.setContent("Test 내용");
+		board.setWriter("Test 유저");
+		
+		boardMapper.insert(board);
+	}
+}
