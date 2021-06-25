acebc6aabe8a54403d4bb26c256348efeb56272f
mika <73530023+2everlove@users.noreply.github.com>
2021-06-25 19:49:01 +0900

    check

--- board/src/main/java/jmp/spring/controller/BoardController.java
+++ board/src/main/java/jmp/spring/controller/BoardController.java
@@ -3,7 +3,6 @@ package jmp.spring.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
